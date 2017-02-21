8e2b3ebe0feb222458453a8b3186c90217bac581
ABoK4Do <alesha148@gmail.com>
2017-02-21 16:08:20 +0300

    New class ResultPOJO

--- src/classes/ResultPOJO.java
+++ src/classes/ResultPOJO.java
@@ -6,13 +6,45 @@ import java.util.ArrayList;
  * Created by ABoK4Do on 21.02.17.
  */
 public class ResultPOJO {
-    private ArrayList info = null;
+    private int id;
+    private String name;
+    private int cat_id;
+    private float price;
 
     public ResultPOJO(){};
-    public void setResultPOJO(ArrayList info){
-        this.info = info;
+
+    //id
+    public void setId(int id){
+        this.id = id;
     }
-    public ArrayList getResultPOJO(){
-        return this.info;
+    public int getId(){
+        return this.id;
     }
+
+    //name
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    //cat_id
+    public void setCat_id(int cat_id){
+        this.cat_id = cat_id;
+    }
+    public int getCat_id(){
+        return this.cat_id;
+    }
+
+    //price
+    public float setPrice(float price){
+        this.price = price;
+    } public float getPrice(){
+        return this.price;
+    }
+
+
+
+
 }
